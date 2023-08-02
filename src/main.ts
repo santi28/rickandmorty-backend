66167a7da8574d53f460a8c0be4897763b0d9f79
Santiago de Nicolás <santidenicolas@gmail.com>
2023-08-02 17:43:19 -0300

    added router

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 
+import routes from './routes/index.router'
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api', routes)
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000')
---
+++ src/routes/index.router.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import charactersRouter from './characters.router';
+
+const router = Router();
+
+router.use('/characters', charactersRouter);
+
+export default router;
